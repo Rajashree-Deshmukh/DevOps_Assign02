9dbfbca9d272d3dd572911b4ae65ced61a5620c0
Rajashree-Deshmukh <67226445+Rajashree-Deshmukh@users.noreply.github.com>
2021-03-16 16:48:17 +0530

    Add files via upload

---
+++ student.sql
@@ -0,0 +1,15 @@
+use pucsdStudent;
+
+CREATE TABLE Student (
+  rollno INTEGER PRIMARY KEY,
+    name VARCHAR(20),
+    address VARCHAR(20),
+    mobno INTEGER,
+    PANno INTEGER
+);
+
+insert into Student values(101,"Rajashree","Rahuri",9890767588,1837959);
+insert into Student values(102,"Jayesh","Rahuri",9882128490,8980768);
+insert into Student values(103,"Rahul","Nashik",8380066351,6754789);
+insert into Student values(104,"Priti","Kolhapur",7588179348,9535282);
+insert into Student values(105,"Ameay","Pune",9822729173,9376283);
